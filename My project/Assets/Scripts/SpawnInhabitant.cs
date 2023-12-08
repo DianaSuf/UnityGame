40b23e650d374652c5bd7032884d84acf8807bf4
DianaSuf <129599628+DianaSuf@users.noreply.github.com>
2023-12-09 03:07:53 +0500

    исправила ошибки

--- My project/Assets/Scripts/SpawnInhabitant.cs
+++ My project/Assets/Scripts/SpawnInhabitant.cs
@@ -7,13 +7,15 @@ public class SpawnInhabitant : MonoBehaviour
 {
     public GameObject spawnInhabitant;
     public GameObject spawnPoint;
+    private bool spawning = true;
 
     void Update()
     {
-        if (Noise.MaxScaleNoise)
+        if (Noise.MaxScaleNoise && spawning)
         {
             Instantiate(spawnInhabitant, spawnPoint.transform.position, Quaternion.identity);
             Destroy(spawnPoint);
+            spawning = false;
         }
 
     }
